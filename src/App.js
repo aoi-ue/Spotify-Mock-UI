49aa345f01fae71de014d805f69fcec6d6f81623
yuhanyuhan <33086695+yuhanyuhan@users.noreply.github.com>
2022-08-20 12:23:19 +0800

    Set up Fetch Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./assets/styles.css";
 
+import SelectedSlider from "./components/Slider";
+
 export default function App() {
 	return (
 		<div className="App">
+			<SelectedSlider />
 			<h1>Hello CodSandbox</h1>
 			<h2>Start editing to see some magic happen!</h2>
 		</div>
---
+++ src/components/Slider.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const USER_SERVICE_URL = "https://jsonplaceholder.typicode.com/users";
+
+export default function SelectedSlider() {
+	const [data, setData] = useState({ users: [], isFetching: false });
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				setData({ users: data.users, isFetching: true });
+				const response = await axios.get(USER_SERVICE_URL);
+				setData({ users: response.data, isFetching: false });
+			} catch (e) {
+				console.log(e);
+				setData({ users: data.users, isFetching: false });
+			}
+		};
+		fetchUsers();
+	}, []);
+
+	return (
+		<ul>
+			{console.log(data)}
+			{data.users.map((el) => (
+				<li key={el.id}> {el.name} </li>
+			))}
+		</ul>
+	);
+}
