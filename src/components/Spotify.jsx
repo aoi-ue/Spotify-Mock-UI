060c285d7fb49ecded92af9266cd195c6bf0edfb
yuhanyuhan <yuhanlian@gmail.com>
2022-08-21 20:28:25 +0800

    fixed auth expiry issue

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -9,45 +9,68 @@ const {
 // const REDIRECT_URI = "https://hfxd8g.csb.app/callback"
 const RESPONSE_TYPE = "token";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-
 const USER_SERVICE_URL = "https://api.spotify.com/v1/browse/new-releases";
 
-export default function Spotify() {
+// const removeToken = () => {
+// 	setToken("");
+// 	window.localStorage.removeItem("token");
+// };
+
+const ACCESS_TOKEN_KEY = "access_token";
+const EXPIRES_AT_KEY = "expires_at";
+
+function useAuth() {
 	const [token, setToken] = useState(null);
-	const [tokenexp, setTokenexp] = useState(null);
+
+	useEffect(() => {
+		let currentAccessToken = window.localStorage.getItem(ACCESS_TOKEN_KEY); 
+		let currentExpiresAt = window.localStorage.getItem(EXPIRES_AT_KEY);
+
+		let currentTime = Date.now() / 1000;
+		if (currentExpiresAt <= currentTime) {
+			currentAccessToken = null;
+
+			window.localStorage.removeItem(ACCESS_TOKEN_KEY, null);
+			window.localStorage.removeItem(EXPIRES_AT_KEY, null);
+		}
+
+		const hash = window.location.hash;
+
+		if (!currentAccessToken && hash) {
+			const authResponse = new URLSearchParams(hash.substring(1));
+
+			const accessToken = authResponse.get("access_token");
+			const expiresIn = authResponse.get("expires_in");
+
+			if (accessToken && expiresIn) {
+				window.location.hash = "";
+				window.localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+
+				// Expires at contains actual expiration time.
+				const expiresAt = expiresIn + (Date.now() / 1000);
+				window.localStorage.setItem(EXPIRES_AT_KEY, expiresAt)
+			}
+		}
+		setToken(currentAccessToken);
+	}, []);
+
+	return {
+		token
+	}
+}
+
+export default function Spotify() {
+	const { token } = useAuth();
 	const [data, setData] = useState({ users: [], isFetching: false });
 
 	useEffect(() => {
-		const hash = window.location.hash;
-		let token = window.localStorage.getItem("token");
-
-		if (!token && hash) {
-			token = hash
-				.substring(1)
-				.split("&")
-				.find((elem) => elem.startsWith("access_token"))
-				.split("=")[1];
-
-			window.location.hash = "";
-			window.localStorage.setItem("token", token);
-		}
-
-		setToken(token);
-	}, []);
-
-	useEffect(() => {
-		const fetchUsers = async () => {
+		const fetchAlbum = async () => {
 			try {
 				setData({ users: data.users, isFetching: true });
 				const response = await axios.get(USER_SERVICE_URL, {
 					headers: {
 						Authorization: `Bearer ${token}`
 					}
-					// 	,
-					//   params: {
-					// 		q: searchKey,
-					// 		type: "artist"
-					// }
 				});
 				setData({ users: response.data, isFetching: false });
 			} catch (e) {
@@ -55,13 +78,11 @@ export default function Spotify() {
 				setData({ users: data.users, isFetching: false });
 			}
 		};
-		fetchUsers();
-	}, []);
 
-	const logout = () => {
-		setToken("");
-		window.localStorage.removeItem("token");
-	};
+		if (token) {
+			fetchAlbum();
+		}
+	}, [token]);
 
 	return (
 		<div>
@@ -74,18 +95,18 @@ export default function Spotify() {
 					Login to Spotify
 				</a>
 			) : (
-				<button onClick={logout}>Logout</button>
-				// console.log(data)
-				// data.users.albums.items.map((artist) => (
-				// 	<div key={artist.id}>
-				// 		{artist.images.length ? (
-				// 			<img width={"100%"} src={artist.images[0].url} alt="" />
-				// 		) : (
-				// 			<div>No Image</div>
-				// 		)}
-				// 		{artist.name}
-				// 	</div>
-				// ))
+				// <button onClick={logout}>Logout</button>
+				// console.log("check data:", data)
+				data.users.albums.items.map((artist) => (
+					<div key={artist.id}>
+						{artist.images.length ? (
+							<img width={"100%"} src={artist.images[0].url} alt="" />
+						) : (
+							<div>No Image</div>
+						)}
+						{artist.name}
+					</div>
+				))
 			)}
 		</div>
 	);
